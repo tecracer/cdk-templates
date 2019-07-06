95c79dd18ac8f23a2d04f92c36a215af0240a51b
Gernot Glawe <gglawe@tecracer.de>
2019-07-06 08:46:41 +0200

    Updated to cdk 0.37

--- alb-update/index.ts
+++ alb-update/index.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import autoscaling = require('@aws-cdk/aws-autoscaling');
-import ec2 = require('@aws-cdk/aws-ec2');
-import iam = require('@aws-cdk/aws-iam');
+import {AmazonLinuxImage,InstanceClass,InstanceSize,InstanceType} from '@aws-cdk/aws-ec2';
+import { ManagedPolicy,Role,ServicePrincipal} from '@aws-cdk/aws-iam';
 import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
-import cdk = require('@aws-cdk/cdk');
-import { AmazonLinuxGeneration } from '@aws-cdk/aws-ec2';
+import cdk = require('@aws-cdk/core');
+import { AmazonLinuxGeneration , Vpc} from '@aws-cdk/aws-ec2';
 
 import fs = require('fs');
 
@@ -12,19 +12,20 @@ class LoadBalancerStack extends cdk.Stack {
   constructor(app: cdk.App, id: string) {
     super(app, id);
 
-    const vpc = new ec2.Vpc(this, 'VPC');
+    const albVpc = new Vpc(this, 'VPC');
   
 
-    const instanceRole = new iam.Role(this,'webinstancerole',
+    const instanceRole = new Role(this,'webinstancerole',
     {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
-      managedPolicyArns: ['arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM'],
+      assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonEC2RoleforSSM')],
+      
     });
 
     const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
-      vpc,
-      instanceType: new ec2.InstanceTypePair(ec2.InstanceClass.Burstable2, ec2.InstanceSize.Micro),
-      machineImage: new ec2.AmazonLinuxImage({generation: AmazonLinuxGeneration.AmazonLinux2}),
+      vpc: albVpc,
+      instanceType: InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO),
+      machineImage: new AmazonLinuxImage({generation: AmazonLinuxGeneration.AMAZON_LINUX_2}),
       role: instanceRole,
     });
 
@@ -34,7 +35,7 @@ class LoadBalancerStack extends cdk.Stack {
     asg.addUserData(bootscript);
 
     const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
-      vpc,
+      vpc: albVpc,
       internetFacing: true
     });
 
@@ -57,4 +58,3 @@ class LoadBalancerStack extends cdk.Stack {
 
 const app = new cdk.App();
 new LoadBalancerStack(app, 'LoadBalancerStack');
-app.run();
