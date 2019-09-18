0fdbb444a600b236c77a23869432579cc8e69f8e
Gernot Glawe <gglawe@tecracer.de>
2019-09-18 14:47:30 +0200

    Updated test

--- r53/test/r53.test.ts
+++ r53/test/r53.test.ts
@@ -1,4 +1,4 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, MatchStyle, haveResource } from '@aws-cdk/assert';
 import cdk = require('@aws-cdk/core');
 import R53 = require('../lib/r53-stack');
 
@@ -7,7 +7,8 @@ test('Empty Stack', () => {
     // WHEN
     const stack = new R53.R53Stack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expectCDK(stack).to(haveResource('AWS::Route53::HostedZone',
+     {
+          "Name": "mydomain.de.",
+    }));
 });
