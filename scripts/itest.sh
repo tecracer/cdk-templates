82853b4aaa261f8c7be125bb0f9694dc5df20470
Gernot Glawe <gglawe@tecracer.de>
2021-01-08 11:10:08 +0100

    Tested with cdk 1.83

--- scripts/itest.sh
+++ scripts/itest.sh
@@ -6,8 +6,24 @@
 npm install cdk -g
 mkdir -p log
 echo off
-echo "Testing all"
- for i in alb-update aws-custom-resource-ses ec2-autostop ec2-instanceconnect lambda-apigw lambda-schedule lambda-simple lambda-go OpsCenterRole r53 transitgateway vpc-bastion codepipeline iam-user selfEditing
+echo "Testing all without bootstrap"
+ for i in alb-update ec2-autostop OpsCenterRole r53 transitgateway vpc-bastion codepipeline iam-user selfEditing
+do
+    echo Testing $i
+    date
+    cd $i 
+    task edge-of-tomorrow >../log/$i.log 2>../log/$i.err 
+    if [ $? -eq 0 ]
+    then
+        echo ✅ $i OK
+    else
+        echo ❌ $i NOK
+    fi
+    cd ..
+    echo "##############################"
+done
+echo "Testing all with bootstrap"
+ for i in  aws-custom-resource-ses  ec2-instanceconnect lambda-apigw lambda-schedule lambda-simple lambda-go 
 do
     echo Testing $i
     date
