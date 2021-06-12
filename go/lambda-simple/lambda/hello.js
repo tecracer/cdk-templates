25100361d402d62334d78854aa5982ca4679366b
Gernot Glawe <gglawe@tecracer.de>
2021-06-12 16:32:39 +0200

    Force commit of lambda.js file (ignored from typescript, because generated)

---
+++ go/lambda-simple/lambda/hello.js
@@ -0,0 +1,10 @@
+exports.handler = async function(event) {
+    console.log('request:', JSON.stringify(event, undefined, 2));
+      return {
+        statusCode: 200,
+        headers: { 'Content-Type': 'text/plain' },
+        body: `Hello, CDK 1.53 again! You've hit ${event.path}\n`
+      };
+    };
+    
+  
